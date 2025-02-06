36c93e3d61ebd1698e9c070dbdf8f596bf3bc018
Ajeetx24 <heropanther9@gmail.com>
2025-02-06 18:56:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001536135490"))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001591180536"))
-ADMINS = int(environ.get("ADMINS", "5935267941 1923770971"))
+ADMINS = int(environ.get("ADMINS", "5935267941"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "autoacceptbot")
+DB_NAME = environ.get("DB_NAME", "MONTY")
 
 # If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
