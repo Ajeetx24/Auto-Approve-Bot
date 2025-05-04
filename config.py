56e91fdfb8865cf102f0fdf15d028b673bd00d76
Ajeetx24 <heropanther9@gmail.com>
2025-05-04 20:48:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "9328697"))
-API_HASH = environ.get("API_HASH", "9844b5bade2267c9a175a1dc72952e76")
+API_ID = int(environ.get("API_ID", "24005775"))
+API_HASH = environ.get("API_HASH", "e2593d8b0f5f52798926619defc21905")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001547058298"))
