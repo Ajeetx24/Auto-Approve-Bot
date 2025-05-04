0e191fb82d65815d274ad8bb988c82dd263ad469
Ajeetx24 <heropanther9@gmail.com>
2025-05-04 21:23:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ API_ID = int(environ.get("API_ID", "24005775"))
 API_HASH = environ.get("API_HASH", "e2593d8b0f5f52798926619defc21905")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001547058298"))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001591180536"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001516086171"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002400433284"))
 ADMINS = int(environ.get("ADMINS", "5935267941"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
